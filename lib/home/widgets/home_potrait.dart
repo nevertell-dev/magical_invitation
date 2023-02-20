118d0ef1d2941d939253f8be743bc086b4a04c76
Nevertell <logicraftmail@gmail.com>
2023-02-20 18:00:27 +0700

    attemp 2

--- lib/home/widgets/home_potrait.dart
+++ lib/home/widgets/home_potrait.dart
@@ -23,45 +23,47 @@ class HomePotrait extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Positioned(
-        height: size.height * 2,
-        width: size.width,
-        child: Stack(
-          alignment: Alignment.topCenter,
+    return Stack(
+      fit: StackFit.expand,
+      alignment: Alignment.topCenter,
+      children: [
+        Positioned(
+          width: size.width,
+          height: size.height * 2,
+          child: Image.asset(
+            'assets/images/$image',
+            alignment: Alignment.topCenter,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Column(
           children: [
-            Image.asset(
-              'assets/images/$image',
-              alignment: Alignment.topCenter,
-              fit: BoxFit.fitWidth,
-            ),
-            Column(
-              children: [
-                Text(title,
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      height: 0.8,
-                      fontSize: 64,
-                      fontFamily: 'PlayfairDisplay',
-                      fontWeight: FontWeight.w800,
-                      color: Color(0xFF0F133A),
-                    )),
-                const SizedBox(height: 16.0),
-                Text(caption,
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      height: 0.8,
-                      fontSize: 24,
-                      fontFamily: 'GreatVibes',
-                      fontWeight: FontWeight.w800,
-                      color: Color(0xFF0F133A),
-                    )),
-              ],
-            ).animate(adapter: _textAdapter).moveY(begin: 500, end: 250),
+            Text(title,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                style: const TextStyle(
+                  height: 0.8,
+                  fontSize: 64,
+                  fontFamily: 'PlayfairDisplay',
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xFF0F133A),
+                )),
+            const SizedBox(height: 16.0),
+            Text(caption,
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                style: const TextStyle(
+                  height: 0.8,
+                  fontSize: 24,
+                  fontFamily: 'GreatVibes',
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xFF0F133A),
+                )),
           ],
-        )
-            .animate(adapter: _potraitAdapter)
-            .moveY(begin: size.height, end: -size.height));
+        ).animate(adapter: _textAdapter).moveY(begin: 500, end: 250),
+      ],
+    )
+        .animate(adapter: _potraitAdapter)
+        .moveY(begin: size.height, end: -size.height);
   }
 }
